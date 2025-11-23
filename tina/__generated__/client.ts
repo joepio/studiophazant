9d815ca2982061649ee21ff88c3624684a58cab0
Joep Meindertsma <joep@ontola.io>
2025-11-23 16:06:33 +0100

    Update TinaCMS client to use env var for URL

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+const url = process.env.NEXT_PUBLIC_TINA_GRAPHQL_URL || 'http://localhost:4001/graphql';
+export const client = createClient({ url, token: process.env.TINA_TOKEN, queries,  });
 export default client;
-  
